cec7ae6bec9a05fe08c14fdd722f4fc5fa6bb824
desmo <62178867+desmodrone@users.noreply.github.com>
2023-10-03 15:37:36 +0200

    process_instruction outline

--- app/src/main.rs
+++ app/src/main.rs
@@ -5,4 +5,22 @@ fn main() {
         pub campaign_description: String,
         pub campaign_fulfilled: u64,
     }
+
+    pub fn process_instruction(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        data: &[u8],
+    ) -> ProgramResult {
+        let (instruction_byte, all_other_bytes) = data.split_first().unwrap();
+
+        if *instruction_byte == 0 {
+            //create campaign
+        } else if *instruction_byte == 1 {
+            //fund campaing
+        } else if *instruction_byte == 2 {
+            // get how many funds are left to reach the requested amount
+        } else if *instruction_byte == 3 {
+            // withdraw all the collected funds and close campaign
+        }
+    }
 }
