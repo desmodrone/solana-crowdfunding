93b861cbfe37c4fb7ac5e2019f679af42853a5ed
desmo <62178867+desmodrone@users.noreply.github.com>
2023-10-03 16:29:50 +0200

    original code added to app side

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,44 +1,69 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use std::collections::HashMap;
+use std::convert::TryInto;
+
 use solana_program::{
-    account_info::{next_acount_info, AccountInfo},
+    account_info::{next_account_info, AccountInfo},
     entrypoint,
     entrypoint::ProgramResult,
     msg,
-    program_error::ProgramErorr,
-    pubkey::Pubkey
-    rent::Rent,
-    sysvar::Sysvar,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct CampaignAccount {
+    pub campaign_owner: Pubkey,
+    pub campaign_amount: u64,
+    pub campaign_description: String,
+    pub campaign_fulfilled: u64,
 }
 
-    pub struct CampaignAccount {
-        pub campaign_owner: Pubkey,
-        pub campaign_amount: u64,
-        pub campaign_description: String,
-        pub campaign_fulfilled: u64,
-    }
+entrypoint!(process_instruction);
 
-    pub fn process_instruction(
-        program_id: &Pubkey,
-        accounts: &[AccountInfo],
-        data: &[u8],
-    ) -> ProgramResult {
-        let (instruction_byte, all_other_bytes) = data.split_first().unwrap();
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    data: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let campaign_account = next_account_info(accounts_iter)?;
 
-        if *instruction_byte == 0 {
-            //create campaign
-        } else if *instruction_byte == 1 {
-            //fund campaing
-        } else if *instruction_byte == 2 {
-            // get how many funds are left to reach the requested amount
-        } else if *instruction_byte == 3 {
-            // withdraw all the collected funds and close campaign
-        }
-    }
+    let (instruction_byte, rest_of_data) = data.split_first().unwrap();
 
-    let iterable_accounts = &mut accounts.iter();
-    let campaign_account = next_account_info(iterable_accounts);
-
-    let amout = rest_of_data
+    let amount = rest_of_data
         .get(..8)
         .and_then(|slice| slice.try_into().ok())
         .map(u64::from_le_bytes)
         .unwrap();
+
+    let description = String::from_utf8(rest_of_data[9..].to_vec()).unwrap();
+
+    if *instruction_byte == 0 {
+        let campaign_owner_account = next_account_info(accounts_iter)?;
+        let mut campaign_account_data =
+            CampaignAccount::try_from_slice(&campaign_account.data.borrow())?;
+        campaign_account_data.campaign_owner = *campaign_owner_account.owner;
+        campaign_account_data.campaign_amount = amount;
+        campaign_account_data.campaign_description = description;
+        campaign_account_data.campaign_fulfilled = 0;
+        campaign_account_data.serialize(&mut &mut campaign_account.data.borrow_mut()[..])?;
+    }
+
+    if *instruction_byte == 1 {
+        //get campaign status
+
+        let mut campaign_account_data =
+            CampaignAccount::try_from_slice(&campaign_account.data.borrow())?;
+        msg!(
+            "{}",
+            campaign_account_data.campaign_amount - campaign_account_data.campaign_fulfilled
+        );
+    }
+
+    if *instruction_byte == 2 {}
+
+    if *instruction_byte == 3 {}
+
+    Ok(())
+}
