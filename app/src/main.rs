85468fe980cdf0dd8e726149b97d9ef082a60b52
desmo <62178867+desmodrone@users.noreply.github.com>
2023-10-03 15:30:42 +0200

    first commit

---
+++ app/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    pub struct CampaignAccount {
+        pub campaign_owner: Pubkey,
+        pub campaign_amount: u64,
+        pub campaign_description: String,
+        pub campaign_fulfilled: u64,
+    }
+}
---
+++ app/target/debug/deps/app-05f5a2a1dac38bd7.d
@@ -0,0 +1,5 @@
+/Users/desmo/repos/solana-crowdfunding/app/target/debug/deps/app-05f5a2a1dac38bd7.rmeta: src/main.rs
+
+/Users/desmo/repos/solana-crowdfunding/app/target/debug/deps/app-05f5a2a1dac38bd7.d: src/main.rs
+
+src/main.rs:
