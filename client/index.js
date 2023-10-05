c27efddb7d9714d44ddbd48007ab6ed55d94f988
desmo <62178867+desmodrone@users.noreply.github.com>
2023-10-05 16:03:01 +0200

    relaunch solana app

--- client/index.js
+++ client/index.js
@@ -1,89 +1,119 @@
 // Import necessary libraries
+const fs = require('fs');
 const {
     Connection,
     Keypair,
     PublicKey,
     Transaction,
-    SystemProgram,
     TransactionInstruction,
+    SystemProgram,
+    sendAndConfirmTransaction,
+    clusterApiUrl,
 } = require('@solana/web3.js');
-const fs = require('mz/fs');
-const os = require('os');
-const path = require('path');
+const borsh = require('borsh');
 
-// Connection setup
-const rpcUrl = 'http://127.0.0.1:8899';
-const connection = new Connection(rpcUrl, 'confirmed');
-
-// Example programId. Replace with actual deployed program's id
-const programId = new PublicKey('2N9QwVW2HiDBnVpq4jFMEt3Q66w8D8udiBb7xXNVgGga');
-
-// Establish connection and log the version
-async function establishConnection() {
-    const version = await connection.getVersion();
-    console.log('Connection to cluster established:', rpcUrl, version);
+// Define the structure of the data that will be stored on-chain
+class CampaignDetails {
+    constructor() {
+        this.admin = new Uint8Array(32);  // Assuming admin is a public key represented as bytes
+        this.name = '';
+        this.description = '';
+        this.image_link = '';
+        this.amount_donated = 0n;  // Using BigInt for u64
+    }
 }
 
 
-// Fetch Keypair from a local file
-async function createKeypairFromFile() {
-    const idPath = path.join(os.homedir(), '.config', 'solana', 'id.json');
-    const secretKeyString = await fs.readFile(idPath, {encoding: 'utf8'});
-    const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
-    return Keypair.fromSecretKey(secretKey);
-}
+const CampaignSchema = new Map([
+    [CampaignDetails, {
+        kind: 'struct',
+        fields: [
+            ['admin', [32]],
+            ['name', 'string'],
+            ['description', 'string'],
+            ['image_link', 'string'],
+            ['amount_donated', 'u64'],
+        ],
+    }],
+]);
 
-// Implement basic transaction setup, signing, and sending
-async function setPayerAndBlockhashTransaction(instructions) {
-    const transaction = new Transaction();
-    instructions.forEach(instruction => transaction.add(instruction));
-    transaction.feePayer = (await createKeypairFromFile()).publicKey;
+
+// Program Id from your deployment
+const PROGRAM_ID = new PublicKey('9F8PipnJqwqrxgej5BDixa2kTJ8Fuqf4zvBVZ3oVYFuM');
+
+// Connection to the network
+const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+
+// Campaign Creation
+async function createCampaign(campaignDetails, payer) {
+    const campaignAccount = new Keypair();
+    const serializedCampaign = borsh.serialize(CampaignSchema, campaignDetails);
+
+    const dataSize = serializedCampaign.length;
+
+    // Determine the required balance for rent exemption
+    const rentExemption = await connection.getMinimumBalanceForRentExemption(dataSize);
+
+    // 1. Create Account and assign ownership to program
+    const createAccountTx = SystemProgram.createAccount({
+        fromPubkey: payer.publicKey,
+        newAccountPubkey: campaignAccount.publicKey,
+        lamports: rentExemption,
+        space: dataSize,
+        programId: PROGRAM_ID,
+    });
+
+    // 2. Initialize Account with the initial state
+    const initAccountTx = new TransactionInstruction({
+        keys: [
+            { pubkey: campaignAccount.publicKey, isSigner: true, isWritable: true },
+            { pubkey: payer.publicKey, isSigner: true, isWritable: false },
+        ],
+        programId: PROGRAM_ID,
+        data: Buffer.concat([Buffer.from([0]), serializedCampaign]),
+    });
+
+    // Bundle transactions
+    const transaction = new Transaction()
+        .add(createAccountTx)
+        .add(initAccountTx);
+    transaction.feePayer = payer.publicKey;
     transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-    return transaction;
+
+    const signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [payer, campaignAccount],
+        {commitment: 'singleGossip', preflightCommitment: 'singleGossip'},
+    );
+
+    console.log(`Transaction ${signature} confirmed`);
+    console.log(`Campaign Account: ${campaignAccount.publicKey.toBase58()}`);
 }
 
-async function signAndSendTransaction(transaction) {
+
+
+
+// Main Function
+async function main() {
+    const keypairBuffer = fs.readFileSync("/Users/desmo/.config/solana/id.json");
+    const keypairArray = JSON.parse(keypairBuffer.toString());
+    const payer = Keypair.fromSecretKey(new Uint8Array(keypairArray));
+
+    const campaignDetails = new CampaignDetails({
+        admin: payer.publicKey,
+        name: "Test Campaign",
+        description: "This is a test campaign.",
+        image_link: "http://example.com/image.png",
+        amount_donated: 0
+    });
+
     try {
-        const keypair = await createKeypairFromFile();
-        transaction.partialSign(keypair);
-        const signedTransaction = transaction.serialize();
-        const signature = await connection.sendRawTransaction(signedTransaction, {skipPreflight: false, preflightCommitment: 'singleGossip'});
-        console.log('Signature:', signature);
-    } catch (err) {
-        console.error('Error in signAndSendTransaction:', err);
+        await createCampaign(campaignDetails, payer);
+        console.log("Campaign created successfully!");
+    } catch(err) {
+        console.error(err);
     }
 }
 
-
-async function interactWithProgram() {
-    try {
-        // Example public key for new account, seed should be unique per each new account
-        const seed = 'UniqueSeed123';
-        const newAccountPubkey = await PublicKey.createWithSeed(
-            (await createKeypairFromFile()).publicKey,
-            seed,
-            programId,
-        );
-
-        // instruction
-        const instructionData = Uint8Array.of(0);
-        console.log('Sending transaction with data:', Buffer.from(instructionData)); 
-        
-        const instruction = new TransactionInstruction({
-            keys: [{pubkey: newAccountPubkey, isSigner: false, isWritable: true}],
-            programId: programId,
-            data: Buffer.from(instructionData),
-        });
-
-        const transaction = await setPayerAndBlockhashTransaction([instruction]);
-        await signAndSendTransaction(transaction);
-
-    } catch (err) {
-        console.error('Error in interactWithProgram:', err);
-    }
-}
-
-// Run methods
-establishConnection().then(() => {
-    interactWithProgram().catch(err => console.error(err));
-});
+main();
--- src/crowdfunding/src/lib.rs
+++ src/crowdfunding/src/lib.rs
@@ -1,6 +1,4 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use std::convert::TryInto;
-
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
@@ -8,87 +6,162 @@ use solana_program::{
     msg,
     program_error::ProgramError,
     pubkey::Pubkey,
+    rent::Rent,
+    sysvar::Sysvar,
 };
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    if instruction_data.len() == 0 {
+        return Err(ProgramError::InvalidInstructionData);
+    }
 
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct CampaignAccount {
-    pub campaign_owner: Pubkey,
-    pub campaign_amount: u64,
-    pub campaign_description: String,
-    pub campaign_fulfilled: u64,
+    if instruction_data[0] == 0 {
+        return create_campaign(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()],
+        );
+    } else if instruction_data[0] == 1 {
+        return withdraw(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()],
+        );
+    } else if instruction_data[0] == 2 {
+        return donate(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()],
+        );
+    }
+    msg!("Didn't found the entrypoint required");
+    Err(ProgramError::InvalidInstructionData)
 }
-
 entrypoint!(process_instruction);
 
-pub fn process_instruction(
-    program_id: &Pubkey, 
-    accounts: &[AccountInfo], 
-    data: &[u8], 
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+struct CampaignDetails {
+    pub admin: [Pubkey],
+    pub name: String,
+    pub description: String,
+    pub image_link: String,
+    pub amount_donated: u64,
+}
+
+fn create_campaign(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
 ) -> ProgramResult {
-
     let accounts_iter = &mut accounts.iter();
-    let campaign_account = next_account_info(accounts_iter)?;
-
-    let (instruction_byte, rest_of_data) = data.split_first().unwrap();
-
-    let amount = rest_of_data
-      .get(..8)
-      .and_then(|slice| slice.try_into().ok())
-      .map(u64::from_le_bytes)
-      .unwrap();
-      
-    let description = String::from_utf8(rest_of_data[9..].to_vec()).unwrap();      
-
-    if *instruction_byte == 0 {
-        let campaign_owner_account = next_account_info(accounts_iter)?;
-        let mut campaign_account_data = CampaignAccount::try_from_slice(&campaign_account.data.borrow())?;
-        campaign_account_data.campaign_owner = *campaign_owner_account.owner;
-        campaign_account_data.campaign_amount = amount;
-        campaign_account_data.campaign_description = description;
-        campaign_account_data.campaign_fulfilled = 0;
-        campaign_account_data.serialize(&mut &mut campaign_account.data.borrow_mut()[..])?;
+    let writing_account = next_account_info(accounts_iter)?;
+    let creator_account = next_account_info(accounts_iter)?;
+    if !creator_account.is_signer {
+        msg!("creator_account should be signer");
+        return Err(ProgramError::IncorrectProgramId);
     }
 
-    else if *instruction_byte == 1 {
-        // Get campaign status 
-        let campaign_account_data = CampaignAccount::try_from_slice(&campaign_account.data.borrow())?;
-        msg!("Remaining amount to fulfill the campaign: {}", campaign_account_data.campaign_amount - campaign_account_data.campaign_fulfilled);
+    if writing_account.owner != program_id {
+        msg!("writing_account isn't owned by program");
+        return Err(ProgramError::IncorrectProgramId);
     }
 
-    else if *instruction_byte == 2 {
-        // Example: Update campaign status 
-        let mut campaign_account_data = CampaignAccount::try_from_slice(&campaign_account.data.borrow())?;
-        // Check the campaign status and update if necessary (logic to be added per requirements)
-        // Update the on-chain campaign data
-        campaign_account_data.serialize(&mut &mut campaign_account.data.borrow_mut()[..])?;
+    let mut input_data = CampaignDetails::try_from_slice(&instruction_data)
+        .expect("Instruction data serialization didn't worked");
+
+    if input_data.admin != *creator_account.key {
+        msg!("Invaild instruction data");
+        return Err(ProgramError::InvalidInstructionData);
+    }
+    let rent_exemption = Rent::get()?.minimum_balance(writing_account.data_len());
+    if **writing_account.lamports.borrow() < rent_exemption {
+        msg!("The balance of writing_account should be more then rent_exemption");
+        return Err(ProgramError::InsufficientFunds);
+    }
+    input_data.amount_donated = 0;
+    input_data.serialize(&mut &mut writing_account.try_borrow_mut_data()?[..])?;
+    Ok(())
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+struct WithdrawRequest {
+    pub amount: u64,
+}
+
+fn withdraw(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let writing_account = next_account_info(accounts_iter)?;
+    let admin_account = next_account_info(accounts_iter)?;
+
+    if writing_account.owner != program_id {
+        msg!("writing_account isn't owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    if !admin_account.is_signer {
+        msg!("admin should be signer");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    let campaign_data = CampaignDetails::try_from_slice(*writing_account.data.borrow())
+        .expect("Error deserialaizing data");
+
+    if campaign_data.admin != *admin_account.key {
+        msg!("Only the account admin can withdraw");
+        return Err(ProgramError::InvalidAccountData);
+    }
+    let input_data = WithdrawRequest::try_from_slice(&instruction_data)
+        .expect("Instruction data serialization didn't worked");
+
+    let rent_exemption = Rent::get()?.minimum_balance(writing_account.data_len());
+    if **writing_account.lamports.borrow() - rent_exemption < input_data.amount {
+        msg!("Insufficent balance");
+        return Err(ProgramError::InsufficientFunds);
     }
 
-    else if *instruction_byte == 3 {
-        // Example: Withdraw funds from campaign
-        let withdrawer_account = next_account_info(accounts_iter)?;
-        let mut campaign_account_data = CampaignAccount::try_from_slice(&campaign_account.data.borrow())?;
+    **writing_account.try_borrow_mut_lamports()? -= input_data.amount;
+    **admin_account.try_borrow_mut_lamports()? += input_data.amount;
 
-        // Validate that the withdrawer is the campaign owner
-        if campaign_account_data.campaign_owner != *withdrawer_account.key {
-            return Err(ProgramError::Custom(0x01)); // Error: Unauthorized
-        }
+    Ok(())
+}
 
-        // Validate that the campaign is fundable or has been funded 
-        if campaign_account_data.campaign_fulfilled < campaign_account_data.campaign_amount {
-            return Err(ProgramError::Custom(0x02)); // Error: Not fundable or not fully funded
-        }
+fn donate(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let writing_account = next_account_info(accounts_iter)?;
+    let donator_program_account = next_account_info(accounts_iter)?;
+    let donator = next_account_info(accounts_iter)?;
 
-        // Logic for transferring funds to the owner (consider using SPL tokens or lamports)
-        // ...
-
-        // Update the campaign as closed or update the relevant data fields
-        campaign_account_data.campaign_fulfilled = 0; // Or other status updates
-        campaign_account_data.serialize(&mut &mut campaign_account.data.borrow_mut()[..])?;
+    if writing_account.owner != program_id {
+        msg!("writing_account isn't owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    if donator_program_account.owner != program_id {
+        msg!("donator_program_account isn't owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    if !donator.is_signer {
+        msg!("donator should be signer");
+        return Err(ProgramError::IncorrectProgramId);
     }
 
-    else {
-        return Err(ProgramError::Custom(0x03)); // Error: Invalid instruction
-    }
+    let mut campaign_data = CampaignDetails::try_from_slice(*writing_account.data.borrow())
+        .expect("Error deserialaizing data");
+
+    campaign_data.amount_donated += **donator_program_account.lamports.borrow();
+
+    **writing_account.try_borrow_mut_lamports()? += **donator_program_account.lamports.borrow();
+    **donator_program_account.try_borrow_mut_lamports()? = 0;
+
+    campaign_data.serialize(&mut &mut writing_account.data.borrow_mut()[..])?;
 
     Ok(())
 }
