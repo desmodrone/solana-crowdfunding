e9a4acfcd860e5520a421cef283ffdea1403cf5a
desmo <62178867+desmodrone@users.noreply.github.com>
2023-10-05 10:13:25 +0200

    add client code

--- client/index.js
+++ client/index.js
@@ -1,17 +1,86 @@
+// Import necessary libraries
 const {
     Connection,
     Keypair,
-  } = require('@solana/web3.js');
-  const fs   = require('mz/fs');
-  
-  async function establishConnection() {
-    const rpcUrl = 'http://127.0.0.1:8899';
-    connection = new Connection(rpcUrl, 'confirmed');
+    PublicKey,
+    Transaction,
+    SystemProgram,
+    TransactionInstruction,
+} = require('@solana/web3.js');
+const fs = require('mz/fs');
+const os = require('os');
+const path = require('path');
+
+// Connection setup
+const rpcUrl = 'http://127.0.0.1:8899';
+const connection = new Connection(rpcUrl, 'confirmed');
+
+// Example programId. Replace with actual deployed program's id
+const programId = new PublicKey('BASWjFeSUEx83XRSdjn8TASa59HUMytSnv3CfEBChYqL');
+
+// Establish connection and log the version
+async function establishConnection() {
     const version = await connection.getVersion();
     console.log('Connection to cluster established:', rpcUrl, version);
-  }
-  async function createKeypairFromFile() {
-    const secretKeyString = await fs.readFile("~/.config/solana/id.json", {encoding: 'utf8'});
+}
+
+// Fetch Keypair from a local file
+async function createKeypairFromFile() {
+    const idPath = path.join(os.homedir(), '.config', 'solana', 'id.json');
+    const secretKeyString = await fs.readFile(idPath, {encoding: 'utf8'});
     const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
     return Keypair.fromSecretKey(secretKey);
-  }
+}
+
+// Implement basic transaction setup, signing, and sending
+async function setPayerAndBlockhashTransaction(instructions) {
+    const transaction = new Transaction();
+    instructions.forEach(instruction => transaction.add(instruction));
+    transaction.feePayer = (await createKeypairFromFile()).publicKey;
+    transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+    return transaction;
+}
+
+async function signAndSendTransaction(transaction) {
+    try {
+        const keypair = await createKeypairFromFile();
+        transaction.partialSign(keypair);
+        const signedTransaction = transaction.serialize();
+        const signature = await connection.sendRawTransaction(signedTransaction, {skipPreflight: false, preflightCommitment: 'singleGossip'});
+        console.log('Signature:', signature);
+    } catch (err) {
+        console.error('Error in signAndSendTransaction:', err);
+    }
+}
+
+
+// Example interaction with the deployed program
+async function interactWithProgram() {
+    try {
+        // Example public key for new account, seed should be unique per each new account
+        const seed = 'UniqueSeed123';
+        const newAccountPubkey = await PublicKey.createWithSeed(
+            (await createKeypairFromFile()).publicKey,
+            seed,
+            programId,
+        );
+
+        // Example instruction
+        const instruction = new TransactionInstruction({
+            keys: [{pubkey: newAccountPubkey, isSigner: false, isWritable: true}],
+            programId: programId,
+            data: Buffer.from(Uint8Array.of(0)), // Pass data as per your program's expected schema
+        });
+
+        const transaction = await setPayerAndBlockhashTransaction([instruction]);
+        await signAndSendTransaction(transaction);
+
+    } catch (err) {
+        console.error('Error in interactWithProgram:', err);
+    }
+}
+
+// Run methods
+establishConnection().then(() => {
+    interactWithProgram().catch(err => console.error(err));
+});
